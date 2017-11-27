4231eb55506f72bcd3c0205af7e5dc642e64c6a2
Orhan Aygün <orhan.aygun@outlook.com>
2017-11-27 14:46:00 +0300

    DataModel projesi oluşturuldu.

---
+++ Binode/Binode.Data.Model/Icerik.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Binode.Data.Model
+{
+    public class Icerik
+    {
+        public int Id { get; set; }
+        public string Isim { get; set; }
+        public DateTime EklenmeTarihi { get; set; }
+        public string AciklamaNotu { get; set; }
+        public Kategori Kategori { get; set; }
+        public IcerikTipi Tip { get; set; }
+    }
+
+    public enum IcerikTipi
+    {
+        Metin,
+        Pdf,
+        Ses,
+        Video
+    }
+
+    
+}
---
+++ Binode/Binode.Data.Model/Kategori.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binode.Data.Model
+{
+    public class Kategori
+    {
+        public int Id { get; set; }
+        public string Isim { get; set; }
+        public string AciklamaNotu { get; set; }
+        public Kategori UstKategori { get; set; }
+        public List<Kategori> AltKategori { get; set; }
+        public List<Icerik> Icerik { get; set; }
+        public int SiraNumarasi { get; set; }
+        public DateTime EklenmeTarihi { get; set; }
+        public string Ikon { get; set; }
+
+    }
+}
